96be370d03b5299bba7aceaebd18ffea20e4b394
CSI-Sebastian-Roche <89159028+CSI-Sebastian-Roche@users.noreply.github.com>
2021-09-01 22:38:38 -0400

    ACSII

---
+++ Projects/ACSII-initials.py
@@ -0,0 +1,10 @@
+print("""
+RRRR   OOO   CCC  H   H  EEEEE
+R   R O   O C   C H   H  E    
+R   R O   O C     H   H  E    
+RRRR  O   O C     HHHHH  EEE
+R   R O   O C     H   H  E
+R    RO   O C   C H   H  E
+R     R OO   CCC  H   H  EEEEE
+""")
+
