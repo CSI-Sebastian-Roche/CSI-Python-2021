69e1103f3fb9b3b30fbc2314296510aea8564f8e
CSI-Sebastian-Roche <89159028+CSI-Sebastian-Roche@users.noreply.github.com>
2021-09-10 14:35:00 -0400

    Module 4 answered

---
+++ Modules/Module4/Modulo4.py
@@ -0,0 +1,7 @@
+print("What did you have for Breafast?")
+meal = "scramble eggs"
+print(meal)
+print("What was your lunch?")
+meal = "Pizza"
+print("What are you going to eat for diner?")
+meal = "rice with chicken"
