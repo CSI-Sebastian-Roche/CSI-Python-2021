9b6d8f11073f9b6a038f6c2b1451050e8e873878
CSI-Sebastian-Roche <89159028+CSI-Sebastian-Roche@users.noreply.github.com>
2021-09-28 13:54:39 -0400

    Figuras

---
+++ Modules/Module4/Sebastian-Roche-Calculator.py
@@ -0,0 +1,22 @@
+# print("Hello World")
+# print(input("What is yor name?"))
+# myName = input()
+# print("It is good to meet you", myName)
+# print("How old are you?")
+# myAge = input()
+
+print("Area of a triangle")
+print("""
+        /\\
+       /  \\
+      /    \\
+     /      \\
+     --------     
+""")
+base = int(input("what is the base of the triangle? "))
+height = int(input("What is the height of the figure?: "))
+half = int(input("What is the half of the triangle? "))
+half = 0.5
+base = 7
+height = 2
+print("The Area of the triangle is: ", half * base * height)
