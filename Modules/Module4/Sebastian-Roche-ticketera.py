1b598d072b8ee8602ab3e67240bcf26ccfb3764e
CSI-Sebastian-Roche <89159028+CSI-Sebastian-Roche@users.noreply.github.com>
2021-09-28 13:56:22 -0400

    Update Sebastian-Roche-ticketera.py

---
+++ Modules/Module4/Sebastian-Roche-ticketera.py
@@ -0,0 +1,11 @@
+print("Bienvenido al concierto P FKN R de Bad Bunny")
+print("El precio de cada asiento es el siguiente = $125 Arena Standing, $50 Reserved Seating 1, $40 Reserved Seating 2, $30 Reserved Seating
+
+ArenaStanding = int(input("How many tickets sold for Arena Standing? = "))
+ReservedSeating 1 = int(input("How many tickets sold for Reserved Seating 1? = "))
+ReservedSeating 2 = int(input("How many tickets sold for Reserved Seating 2? = "))
+ReservedSeating 3 = int(input("How many tickets sold for Reseverd Seating 3? = "))
+
+def SectionsSales(Arena Satnding, Reserved Seating 1, Reserved Seating 2, Reserved Seating 3")
+print(f"The cost for Arena Standing is: {int(Arena Standing) * 125}")
+print(f"The cost for Reserved Seating 1 is: {int(ReservedSeating 1) * 50}")
