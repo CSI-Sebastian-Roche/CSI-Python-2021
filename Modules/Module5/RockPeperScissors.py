6196dc71fc0ccf30d704cf5d7f2582652fab5945
CSI-Sebastian-Roche <89159028+CSI-Sebastian-Roche@users.noreply.github.com>
2021-10-08 14:55:28 -0400

    RPS

---
+++ Modules/Module5/RockPeperScissors.py
@@ -0,0 +1,27 @@
+import random
+print("Welcome to The Rock, Paper, Scissor, Shot Game")
+RPS = ["rock", "paper", "scissor"]
+
+playerChoice = input("Get ready! Choose one: rock, paper, scissors \n")
+computerChoice = random.choice(RPS)
+print(f"Computer selected: {computerChoice}")
+print(f"Player selected: {playerChoice}")
+
+if(playerChoice == computerChoice):
+    print("You tied")
+
+elif(playerChoice == "rock" and computerChoice == "scissors"):
+    print("Rock crushes Scissor. You just beated the hell out of that guy!")
+elif(playerChoice == "rock" and computerChoice == "paper"):
+    print("You just took the L")
+elif(playerChoice == "scissor" and computerChoice == "paper"):
+    print("Scissor cuts the paper. You just beated the hell out of that guy!")
+elif(playerChoice == "scissor" and computerChoice == "rock"):
+    print("You just took the L")
+elif(playerChoice == "paper" and computerChoice == "rock"):
+    print("Paper covers rock. You just beated the hell out of that guy!")
+elif(playerChoice == "paper" and computerChoice == "scissor"):
+    print("You just took the L")
+
+else:
+    print("Something isn't right, try again")
