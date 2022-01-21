405bd0e80d7641b486ce2f8e96d328fb65280c14
CSI-Sebastian-Roche <89159028+CSI-Sebastian-Roche@users.noreply.github.com>
2022-01-21 16:13:41 -0400

    Create Sebastian-Roche-Hangman.py

---
+++ Projects/Sebastian-Roche-Hangman.py
@@ -0,0 +1,16 @@
+import random
+word_list = ["San Juan", "Ponce", "Caguas", "Cabo Rojo", "Humacao", "Viequez", "Moca", "Mayagüez", "San Sebastian", "Cidra"]
+
+def display_hangman(tries):
+    stages = [ """
+                    --------
+                    |   |
+                    |   O
+                    |  /|\\
+                    |  / \\ 
+                    |
+                    --
+                    """,
+
+
+    ]
