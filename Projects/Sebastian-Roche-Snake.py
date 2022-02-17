f1f32ea3899f0d2b5f897dd679b8760df9156fe6
CSI-Sebastian-Roche <89159028+CSI-Sebastian-Roche@users.noreply.github.com>
2022-02-17 14:55:57 -0400

    Update Sebastian-Roche-Snake.py

--- Projects/Sebastian-Roche-Snake.py
+++ Projects/Sebastian-Roche-Snake.py
@@ -10,10 +10,45 @@ blue= (75, 107, 250) #score
 dis=pygame.display.set_mode((400,300))
 pygame.display.upset()
 pygame.display.set_caption("The Snake game")
+game_over= False
+
+x1 = 150
+y1 = 150
+
+x1_change = 0
+y1_change = 0
+
+clock = pygame.time.Clock() 
+
+
 while not game_over:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             game_over== True 
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    x1_change = -10
+                    y1_change = 0 
+                elif event.key == pygame.K_RIGHT:
+                    x1_change = 10
+                    y1_change = 0
+                elif event.key == pygame.K_UP:
+                    x1_change = 0
+                    y1_change = -10
+                elif event.key == pygame.K_DOWN:
+                    x1_change = 0
+                    y1_change = 10
+
+    x1 += x1_change
+    y1 += y1_change
+    dis.fill(green)
+    pygame.draw.rect(dis, purple,[x1, y1, 10, 10])
+
+
+    pygame.display.update()
+
+    clock.tick(15)
+
 pygame.quit()
 quit()
 
